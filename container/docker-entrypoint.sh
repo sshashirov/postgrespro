f55a5a5cd3ed0870f07b499a1681179b2d2bc072
sshashirov <sshashirov@cleverence.ru>
2020-05-28 22:39:39 +0300

    Update docker-entrypoint.sh

--- container/docker-entrypoint.sh
+++ container/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# usage: file_env VAR [DEFAULT]
+# usage: file_env VAR [DEFAULT]!
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
