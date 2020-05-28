f8d1e3355c780750691756e2f8c58374ba17ef5d
sshashirov <sshashirov@cleverence.ru>
2020-05-28 22:13:55 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-docker run --name postgrespro-1c \
+docker run --name postgrespro \
   --net host \
   --detach \
   --volume postgrespro-1c-data:/data \
   --volume /etc/localtime:/etc/localtime:ro \
   --env POSTGRES_PASSWORD=password \
-  voidzster/postgrespro-1c
+  sshashirov/postgrespro
